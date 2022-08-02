9372d652c26d703a1ea742a7af8301415b16ee35
ActuallyAJL <zero3260@hotmail.com>
2022-08-02 14:38:19 -0500

    delete comments complete

--- eat-purge-livestream/client/src/components/Comments/CommentCard.js
+++ eat-purge-livestream/client/src/components/Comments/CommentCard.js
@@ -1,13 +1,23 @@
 import React, { useState, useEffect } from "react";
-import { CardBody, Col, Card, CardText } from "reactstrap";
+import { CardBody, Col, Card, CardText, Button } from "reactstrap";
+import { deleteComment } from "../../modules/commentManager";
 import { getUserById } from "../../modules/userManager";
+import { getLoggedInUser } from "../../modules/userManager";
 
 export const CommentCard = ({ comment }) => {
   const [postedBy, setPostedBy] = useState({});
+  const [currentUser, setCurrentUser] = useState({});
+  const [isEdittingComment, setIsEdittingComment] = useState(false);
 
   useEffect(() => {
     getUserById(comment.userProfileId).then((blob) => setPostedBy(blob));
+    getLoggedInUser().then((blob) => setCurrentUser(blob));
   }, []);
+
+  const handleDeleteComment = () => {
+    deleteComment(comment.id);
+    window.location.reload();
+  };
 
   return (
     <>
@@ -16,6 +26,20 @@ export const CommentCard = ({ comment }) => {
           <CardBody>
             <CardText>{comment.content}</CardText>
             <CardText>-{postedBy.fullName}</CardText>
+            {currentUser.id == postedBy.id ? (
+              <>
+                <Button
+                  onClick={() => {
+                    setIsEdittingComment(true);
+                  }}
+                >
+                  Edit
+                </Button>
+                <Button onClick={handleDeleteComment}>Delete</Button>
+              </>
+            ) : (
+              ""
+            )}
           </CardBody>
         </Card>
       </Col>
